8a0daef3749dacf6eed1cc48e28ef945a732c8e5
Dionata Bergmann Podewils <67449438+DionataBergmann@users.noreply.github.com>
2024-06-14 11:38:09 -0300

    Revert "Feature/vercel"

--- serverless.ts
+++ serverless.ts
@@ -1,16 +1,16 @@
-const serverlessHttp = require("serverless-http");
-const { NestFactory } = require('@nestjs/core');
-const { AppModule } = require('./src/app.module');
-const express = require('express');
-const { ExpressAdapter } = require('@nestjs/platform-express');
+import serverless from 'serverless-http';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './src/app.module';
+import express, { Express } from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
-const expressApp = express();
+const expressApp: Express = express();
 
 async function bootstrap() {
-    const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-    await nestApp.init();
-    const handler = serverlessHttp(expressApp);
-    return handler;
+  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  await nestApp.init();
+  
+  return serverless(expressApp);
 }
 
-module.exports.handler = bootstrap();
+module.exports.handler = bootstrap().then(handler => handler);
