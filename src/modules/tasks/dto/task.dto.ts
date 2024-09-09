ddcf62c6629b5c0c2582635980ec69983dfb26d5
DionataBergmann <dionata.podewils@outlook.com>
2024-09-09 17:06:37 -0300

    :bug: Fix error when updating task

--- src/modules/tasks/dto/task.dto.ts
+++ src/modules/tasks/dto/task.dto.ts
@@ -1,10 +1,10 @@
 import { FilterableField } from '@nestjs-query/query-graphql';
 import { ObjectType } from '@nestjs/graphql';
-import { BaseDTO } from 'src/modules/base/dto/base.dto';
 import { TaskStatus } from '../taskStatusEnum';
+import { BaseDTO } from 'src/modules/base/dto/base.dto';
 
 @ObjectType('Task')
-export class TaskDTO extends BaseDTO {
+export class TaskDTO extends BaseDTO{
   @FilterableField()
   title: string;
 
@@ -20,6 +20,6 @@ export class TaskDTO extends BaseDTO {
   @FilterableField({ nullable: true })
   userName: string;
 
-  @FilterableField(() => TaskStatus, { defaultValue: TaskStatus.PENDING })
+  @FilterableField(() => TaskStatus) 
   status: TaskStatus;
 }
--- src/modules/tasks/dto/update-task.input.ts
+++ src/modules/tasks/dto/update-task.input.ts
@@ -1,8 +1,12 @@
 import { InputType, Field, ID, PartialType } from '@nestjs/graphql';
 import { CreateTaskInput } from './create-task.input';
+import { TaskStatus } from '../taskStatusEnum';
 
 @InputType()
 export class UpdateTaskInput extends PartialType(CreateTaskInput) {
   @Field(() => ID, { nullable: true })
   id?: string;
+
+  @Field(() => TaskStatus, { nullable: true }) 
+  status?: TaskStatus;
 }
